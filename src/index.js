dab350e6400697eba713faf7a59e25dc697bcb98
Jan Verweij (Liferay) <jan.verweij@liferay.com>
2022-03-04 09:51:21 +0100

    lit components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import ReactDOM from 'react-dom';
 import HelloBar from './routes/hello-bar/pages/HelloBar';
 import HelloFoo from './routes/hello-foo/pages/HelloFoo';
 import HelloWorld from './routes/hello-world/pages/HelloWorld';
-import { SimpleGreeting } from './components/SimpleGreeting';
 import './common/styles/index.scss';
+import { SimpleGreeting } from './components/simple-greeting/SimpleGreeting';
+import { MotionCarousel } from './components/motion-carousel/MotionCarousel';
 
 const App = ({ route }) => {
 	if (route === "hello-bar") {
@@ -16,8 +17,12 @@ const App = ({ route }) => {
 		return <HelloFoo />;
 	}
 
-	if (route === "unused") {
-		return <SimpleGreeting />;
+	if (route === "components") {
+		return 
+			<div>
+				<SimpleGreeting />
+				<MotionCarousel />
+			</div>;
 	}
 
 	return <HelloWorld />;
